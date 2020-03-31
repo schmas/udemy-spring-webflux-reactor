29a58c02e487459ae6b1a27576f1d0ae623737e2
Diego Schmidt <diego.schmidt@mercadolivre.com>
2020-03-31 19:34:26 -0300

    initial commit

---
+++ src/test/java/com/br/schmidt/udemy/reactivespring/LearnReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.schmidt.udemy.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactivespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
