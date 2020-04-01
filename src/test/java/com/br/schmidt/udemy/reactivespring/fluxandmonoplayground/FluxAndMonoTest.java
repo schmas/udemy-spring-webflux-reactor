ae735756ba608084a4331a4fd7786e2b1c3bf8e0
Diego Schmidt <diego.schmidt@mercadolivre.com>
2020-03-31 21:02:34 -0300

    Section 7.15

--- src/test/java/com/br/schmidt/udemy/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/br/schmidt/udemy/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.br.schmidt.udemy.reactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
@@ -64,4 +65,20 @@ public class FluxAndMonoTest {
                     .verify();
     }
 
+    @Test
+    void monoTest() {
+        final Mono<String> stringMono = Mono.just("Spring");
+
+        StepVerifier.create(stringMono.log())
+                    .expectNext("Spring")
+                    .verifyComplete();
+    }
+
+    @Test
+    void monoTest_Error() {
+        StepVerifier.create(Mono.error(new RuntimeException("Exception Occurred")).log())
+                    .expectErrorMatches(throwable -> throwable instanceof RuntimeException
+                            && "Exception Occurred".equals(throwable.getMessage()))
+                    .verify();
+    }
 }
