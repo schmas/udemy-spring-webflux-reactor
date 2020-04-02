c9743543ae8c87d17b015408632f5c5bbc356e9e
Diego Schmidt <diego.schmidt@mercadolivre.com>
2020-04-01 22:00:04 -0300

    Section 7.25

---
+++ src/test/java/com/br/schmidt/udemy/reactivespring/fluxandmonoplayground/ColdAndHotPublisherTest.java
@@ -0,0 +1,40 @@
+package com.br.schmidt.udemy.reactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class ColdAndHotPublisherTest {
+
+    @Test
+    void coldPublisherTest() throws InterruptedException {
+        final Flux<String> stringFlux =
+                Flux.just("A", "B", "C", "D", "E", "F")
+                    .delayElements(Duration.ofSeconds(1));
+
+        stringFlux.subscribe(s -> System.out.println("Subscriber 1 : " + s));
+
+        Thread.sleep(2000);
+
+        stringFlux.subscribe(s -> System.out.println("Subscriber 2 : " + s));
+
+        Thread.sleep(4000);
+    }
+
+    @Test
+    void hotPublisherTest() throws InterruptedException {
+        final Flux<String> stringFlux =
+                Flux.just("A", "B", "C", "D", "E", "F")
+                    .delayElements(Duration.ofSeconds(1));
+
+        final ConnectableFlux<String> connectableFlux = stringFlux.publish();
+        connectableFlux.connect();
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 1 : " + s));
+        Thread.sleep(3000);
+
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 2 : " + s));
+        Thread.sleep(4000);
+    }
+}
