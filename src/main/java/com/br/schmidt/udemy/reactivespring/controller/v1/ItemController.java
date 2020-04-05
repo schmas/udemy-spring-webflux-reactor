ae16b4286b442360490f61f73550384707d22940
Diego Schmidt <diego.schmidt@mercadolivre.com>
2020-04-05 18:35:38 -0300

    Section 58

--- src/main/java/com/br/schmidt/udemy/reactivespring/controller/v1/ItemController.java
+++ src/main/java/com/br/schmidt/udemy/reactivespring/controller/v1/ItemController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,19 @@ public class ItemController {
     public Mono<Void> deleteItem(@PathVariable final String id) {
         return itemReactiveRepository.deleteById(id);
     }
+
+    @PutMapping(ItemConstants.ITEM_END_POINT_V1 + "/{id}")
+    public Mono<ResponseEntity<Item>> updateItem(@PathVariable final String id,
+                                                 @RequestBody final Item item) {
+        return itemReactiveRepository.findById(id)
+                                     .flatMap(currItem -> {
+                                         currItem.setPrice(item.getPrice());
+                                         currItem.setDescription(item.getDescription());
+                                         return itemReactiveRepository.save(currItem);
+                                     })
+                                     .map(ResponseEntity::ok)
+                                     .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
 
 }
