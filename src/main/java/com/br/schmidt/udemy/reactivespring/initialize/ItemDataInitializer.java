8980e59d4382cd04cd953b8b8a01b46cc6e8e6cf
Diego Schmidt <diego.schmidt@mercadolivre.com>
2020-04-05 18:00:28 -0300

    Section 52

---
+++ src/main/java/com/br/schmidt/udemy/reactivespring/initialize/ItemDataInitializer.java
@@ -0,0 +1,41 @@
+package com.br.schmidt.udemy.reactivespring.initialize;
+
+import com.br.schmidt.udemy.reactivespring.document.Item;
+import com.br.schmidt.udemy.reactivespring.repository.ItemReactiveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ItemDataInitializer implements CommandLineRunner {
+
+    private final ItemReactiveRepository itemReactiveRepository;
+
+    @Override
+    public void run(final String... args) throws Exception {
+        initialDataSetUp();
+    }
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats Headphones", 19.99)
+        );
+
+    }
+
+    private void initialDataSetUp() {
+        itemReactiveRepository.deleteAll()
+                              .thenMany(Flux.fromIterable(data()))
+                              .flatMap(itemReactiveRepository::save)
+                              .thenMany(itemReactiveRepository.findAll())
+                              .subscribe(item -> System.out.println("Item inserted from CommandLineRunner : " + item));
+    }
+}
